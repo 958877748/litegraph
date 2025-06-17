8cecbe19933cc26c3acc84a384de094700cf5ba7
player1954 <guolei1994@outlook.com>
2025-06-17 23:29:48 +0800

    Update EquipItem.ts

--- apps/editor/src/circuits/EquipItem.ts
+++ apps/editor/src/circuits/EquipItem.ts
@@ -1,4 +1,4 @@
-import { IComboWidget, LGraph, LGraphNode } from "@gausszhou/litegraph-core";
+import { IComboWidget, INumberWidget, LGraph, LGraphNode } from "@gausszhou/litegraph-core";
 
 export enum EquipType {
     Weapon = 10,
@@ -6,6 +6,29 @@ export enum EquipType {
     BodyArmour = 12,
     Shield = 13,
     Jewellery = 14
+}
+
+export enum CharClass {
+    /**
+     * 通用
+     */
+    Common = 0,
+    /**
+     * 剑士
+     */
+    Swordsman = 1,
+    /**
+     * 魔法师
+     */
+    Mage = 2,
+    /**
+     * 战士
+     */
+    Warrior = 3,
+    /**
+     * 箭手
+     */
+    Archer = 4
 }
 
 export default class EquipItem extends LGraphNode {
@@ -22,37 +45,24 @@ export default class EquipItem extends LGraphNode {
         description: '',
         type: EquipType.Weapon,
         price: 0,
+        class: CharClass.Common,
+        hp: 0,
+        mp: 0,
+        attack: 0,
+        defense: 0,
+        speed: 0
     };
 
     type_widget: IComboWidget
+    price_widget: INumberWidget
+    class_widget: IComboWidget
 
     constructor() {
         super();
-
-        // 添加输出端口 名字 类型
         this.addOutput("Output", "EquipItem");
-
-        // 添加只读的ID显示控件
-        this.addWidget(
-            "text",         // 控件类型
-            "ID",           // 显示名称
-            this.properties.id, // 初始值
-            "id",           // 绑定到 properties.id
-        );
-
-        this.addWidget(
-            "text",         // 控件类型
-            "name",           // 显示名称
-            this.properties.name, // 初始值
-            "name",           // 绑定到 properties.name
-        );
-
-        this.addWidget(
-            "text",         // 控件类型
-            "desc",           // 显示名称
-            this.properties.description, // 初始值
-            "description",           // 绑定到 properties.description
-        );
+        this.addWidget("text", "ID", this.properties.id, "id");
+        this.addWidget("text", "name", this.properties.name, "name");
+        this.addWidget("text", "desc", this.properties.description, "description");
 
         // 添加装备类型选择控件
         this.type_widget = this.addWidget(
@@ -67,10 +77,22 @@ export default class EquipItem extends LGraphNode {
             }
         );
 
-        this.addWidget("number", "price", this.properties.price,
+        this.price_widget = this.addWidget("number", "price", this.properties.price,
             (value: number) => {
                 this.properties.price = Math.round(value)
             }, { precision: 0, step: 1, min: 0 });
+
+        this.class_widget = this.addWidget(
+            "combo",         // 使用下拉框控件
+            "class",      // 显示名称
+            CharClass[this.properties.class], // 初始值
+            (value: string) => {
+                this.properties.class = CharClass[value]
+            },
+            {   // 配置选项             
+                values: Object.keys(CharClass).filter(key => isNaN(Number(key)))
+            }
+        );
     }
 
     findNextAvailableId(): number {
@@ -96,6 +118,8 @@ export default class EquipItem extends LGraphNode {
 
     onConfigure(info: any) {
         this.type_widget.value = EquipType[info.properties.type]
+        this.price_widget.value = info.properties.price
+        this.class_widget.value = CharClass[info.properties.class]
         this.size = this.computeSize()
     }
 }
