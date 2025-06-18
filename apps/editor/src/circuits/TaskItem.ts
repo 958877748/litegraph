0cf2a56230862fe799accad9ab61c3927cdcd2a3
guolei <31437616+958877748@users.noreply.github.com>
2025-06-18 15:22:28 +0800

    Update TaskItem.ts

--- apps/editor/src/circuits/TaskItem.ts
+++ apps/editor/src/circuits/TaskItem.ts
@@ -1,70 +1,49 @@
-import { LGraphNode } from "@gausszhou/litegraph-core";
+import { LGraphNode } from "@gausszhou/litegraph-core"
 
 export default class TaskItem extends LGraphNode {
-    static title = "TaskItem";
-    static desc = "TaskItem";
-    static shape = 1;
-    static title_color = "#012";
-    static registerType = "rpg/TaskItem";
-    static filter = "is_filter";
+    static title = "TaskItem"
+    static desc = "TaskItem"
+    static shape = 1
+    static title_color = "#012"
+    static registerType = "rpg/TaskItem"
+    static filter = "is_filter"
 
     properties = {
         id: -1,
         name: '',
-        description: '',
-    };
+        description: ''
+    }
 
     constructor() {
-        super();
-
-        // 添加输出端口 名字 类型
-        this.addOutput("Output", "TaskItem");
-
-        // 添加只读的ID显示控件
-        this.addWidget(
-            "text",         // 控件类型
-            "ID",           // 显示名称
-            this.properties.id, // 初始值
-            "id",           // 绑定到 properties.id
-        ).disabled = true;
-
-        this.addWidget(
-            "text",         // 控件类型
-            "name",           // 显示名称
-            this.properties.name, // 初始值
-            "name",           // 绑定到 properties.name
-        );
-
-        this.addWidget(
-            "text",         // 控件类型
-            "desc",           // 显示名称
-            this.properties.description, // 初始值
-            "description",           // 绑定到 properties.description
-        );
+        super()
+        this.addOutput("Output", "TaskItem")
+        this.addWidget("text", "ID", this.properties.id, "id",).disabled = true
+        this.addWidget("text", "name", this.properties.name, "name")
+        this.addWidget("text", "desc", this.properties.description, "description")
     }
 
     findNextAvailableId(): number {
         const usedIds = new Set<number>();
         for (const node of this.graph._nodes) {
             if (node instanceof TaskItem) {
-                usedIds.add(node.properties.id);
+                usedIds.add(node.properties.id)
             }
         }
         // 从1开始找未用的id
-        let id = 1;
-        while (usedIds.has(id)) id++;
-        return id;
+        let id = 1
+        while (usedIds.has(id)) id++
+        return id
     }
 
     onAdded() {
         if (this.properties.id === -1) {
             // 如果id是-1，说明是新建的item，需要分配一个新的id
-            const newId = this.findNextAvailableId();
-            this.setProperty('id', newId);
+            const newId = this.findNextAvailableId()
+            this.setProperty('id', newId)
         }
     }
 
-    onConfigure(info: any) {
+    onConfigure() {
         this.size = this.computeSize()
     }
 }
